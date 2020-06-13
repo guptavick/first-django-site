7585fc06003017cfa022aa0a406359011fa6306d
vishavgupta-eros <vishav.gupta@erosnow.com>
2020-06-13 15:48:34 +0530

    issue: Added the search functionality and made changes in the templates

--- mysitefirst/myapp/views.py
+++ mysitefirst/myapp/views.py
@@ -1,13 +1,56 @@
 from django.shortcuts import render
-from bs4 import beautifulsoup
+from bs4 import BeautifulSoup
 import requests
+from requests.compat import quote_plus
+from .models import Search
 
 # Create your views here.
+
+BASE_CRAIGLIST_URL = 'https://delhi.craigslist.org/search/?query={}'
+BASE_IMAGE_URL = 'https://images.craigslist.org/{}_300x300.jpg'
 
 def home(request):
     return render(request, 'base.html')
 
 def my_search(request):
     search = request.POST.get('search')
-    stuff_for_frontend = {'search': search}
+
+    Search.objects.create(search = search)
+
+    FINAL_URL = BASE_CRAIGLIST_URL.format(quote_plus(search))
+    print(FINAL_URL)
+
+    response = requests.get(FINAL_URL)
+    data = response.text
+
+    soup = BeautifulSoup(data, features = 'html.parser')
+
+    # post_titles = soup.find_all('a',{ 'class': 'result-title'})
+    post_listings = soup.find_all('li',{ 'class': 'result-row'}) 
+
+    final_postings = []
+    for post in post_listings:
+        post_title = post.find('a',{'class': 'result-title'}).text
+        post_url = post.find('a').get('href')
+
+        if post.find(class_ = 'result-price'):
+            post_price = post.find(class_ = 'result-price').text
+            if post_price == '₨0':
+                post_price = 'NOT MENTIONED'
+        else:
+            post_price = 'N/A'
+    
+        if post.find(class_ = 'result-image').get('data-ids'):
+            post_image_id = post.find(class_ = 'result-image').get('data-ids').split(',')[0].split(':')[1]
+            post_image_url = BASE_IMAGE_URL.format(post_image_id)
+        else:
+            post_image_url = 'https://craigslist.org/images/peace.jpg'
+
+
+        final_postings.append((post_title,post_url,post_price,post_image_url))
+
+    
+    stuff_for_frontend = {'search': search,
+                          'final_postings':final_postings}
+
     return render(request,'myapp/new_search.html', stuff_for_frontend)
